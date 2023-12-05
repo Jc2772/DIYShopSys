2cee1ef751a775f2799ec82b1b579d1ed04bbf4d
T00225039 <james.clifford2@students.ittralee.ie>
2023-12-05 17:27:23 +0000

    .

--- DIYShopSys/MainMenu.cs
+++ DIYShopSys/MainMenu.cs
@@ -19,28 +19,28 @@ namespace DIYShopSys
             ManageData ManageData = new ManageData(this, "Update Supplier Details");
             ManageData.Show();
         }
-        //Remove Supplier Details
+        //Manage Supplier Status
         private void removeSupplierDetailsToolStripMenuItem_Click(object sender, EventArgs e)
         {
             String Text = "Manage Supplier Status";
             ChangeStatus manage = new ChangeStatus(this, Text);
             manage.Show();
         }
-
+        //Add Type
         private void addTypeToolStripMenuItem_Click(object sender, EventArgs e)
         {
             String Text = "Add Type";
             AddAndUpdateTypes AddAndUpdateTypes = new AddAndUpdateTypes(this, Text);
             AddAndUpdateTypes.Show();
         }
-
+        //Update Type
         private void updateTypesToolStripMenuItem_Click(object sender, EventArgs e)
         {
             String Text = "Update Type Detail";
             ManageData manage = new ManageData(this, Text);
             manage.Show();
         }
-
+        //Manage Tool Status
         private void manageTypeStatusToolStripMenuItem_Click(object sender, EventArgs e)
         {
             String Text = "Manage Type Status";
@@ -60,7 +60,7 @@ namespace DIYShopSys
             ManageData ManageData = new ManageData(this, "Update Item Details");
             ManageData.Show();
         }
-        //Remove Item Details
+        //Manage Item Status
         private void removeItemToolStripMenuItem_Click(object sender, EventArgs e)
         {
             String Text = "Manage Item Status";
@@ -85,10 +85,11 @@ namespace DIYShopSys
             YearlyRevenueAnalysis analysis = new YearlyRevenueAnalysis(this);
             analysis.Show();
         }
-
+        //Yearly Item Analysis
         private void yearlyItemAnalysisToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            YearlyItemAnalysis analysis = new YearlyItemAnalysis(this);
+            analysis.Show();
         }
     }
 }
