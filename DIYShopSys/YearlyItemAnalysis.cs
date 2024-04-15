acea96a9ad196b32c03f5ff59356f8a949a8ae98
T00225039 <james.clifford2@students.ittralee.ie>
2024-04-15 16:10:22 +0100

    .

--- DIYShopSys/YearlyItemAnalysis.cs
+++ DIYShopSys/YearlyItemAnalysis.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace DIYShopSys
 {
@@ -25,7 +26,25 @@ namespace DIYShopSys
         }
         private void Year_SelectedIndexChanged(object sender, EventArgs e)
         {
+            //Got Code From Here https://stackoverflow.com/questions/34104484/how-to-build-a-pie-chart-based-on-int-values
+            ItemChart.Series.Clear();
+            ItemChart.Legends.Clear();
+            ItemChart.Legends.Add("MyLegend");
+            ItemChart.Legends[0].LegendStyle = LegendStyle.Table;
+            ItemChart.Legends[0].Docking = Docking.Bottom;
+            ItemChart.Legends[0].Alignment = StringAlignment.Center;
+            ItemChart.Legends[0].Title = "MyTitle";
+            ItemChart.Legends[0].BorderColor = Color.Black;
 
+            string seriesname = "Yearly Item Analysis";
+            ItemChart.Series.Add(seriesname);
+
+            GetData();
+            ProfitChart.Series[0].Points.DataBindXY(months, ammounts);
+        }
+        private void GetData()
+        {
+            DataSet ds = new Sql().GetDataSet("Select Sum(quantity_sold),Type_Name from (items inner join item_types on items.type_id = type.type_id) inner join basket on items.item_id = basket.item_id group by type_name");
         }
         public void GetDate()
         {
